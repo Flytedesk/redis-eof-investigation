9ecc9cf1166462efba74331af5544b4c6f269627
Gary Passero <gpassero@gmail.com>
2025-05-08 12:00:48 -0400

    Initial test case reproduction

---
+++ test.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require "bundler/inline"
+
+gemfile(true) do
+  source "https://rubygems.org"
+
+  gem "rails", "7.2.2.1"
+  # If you want to test against edge Rails replace the previous line with this:
+  # gem "rails", github: "rails/rails", branch: "main"
+
+  gem "sqlite3", "~> 1.4"
+  gem "redis", "5.3.0"
+  gem "redis-client", "0.23.2"
+end
+
+require "active_record"
+require "minitest/autorun"
+require "logger"
+
+# This connection will do for database-independent bug reports.
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+ActiveRecord::Schema.define do
+  create_table :posts, force: true do |t|
+  end
+
+  create_table :comments, force: true do |t|
+    t.integer :post_id
+  end
+end
+
+class Post < ActiveRecord::Base
+  has_many :comments
+end
+
+class Comment < ActiveRecord::Base
+  belongs_to :post
+end
+
+class BugTest < Minitest::Test
+  def test_redis_subscribe
+    redis = Redis.new(url: 'rediss://localhost:6379', ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+    redis.subscribe('supply-side-platform.events') do
+      it.message do
+        Post.exists?(id: rand(2_000_000_000))
+      end
+    end
+  end
+end
